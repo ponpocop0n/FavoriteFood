4cee4d94ffa368000312f19ad5d674bd9f710927
rikoshiomoto <rikoshiomoto@192.168.1.12>
2020-05-16 16:36:51 +0900

    Introduce my favorite food

---
+++ kadai1/src/kadai1/Kadai1_1.java
@@ -0,0 +1,9 @@
+package kadai1;
+public class Kadai1_1 {
+	public static void main(String[] args) {
+		System.out.println("好きな食べ物は、油そばです。");
+		System.out.println("太くてもちもちした麺に旨みたっぷりのタレが絡まって、");
+		System.out.println("口の中に入れたときの多幸感がすごいからです！");
+	}
+}
+
