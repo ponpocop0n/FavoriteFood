8f1b89018fbc2a599153e5de796c3fa36e51998a
megu <megu@localhost>
2020-05-19 12:45:03 +0900

    コメントを追加しました

--- kadai1/src/kadai1/Kadai1_1.java
+++ kadai1/src/kadai1/Kadai1_1.java
@@ -4,6 +4,7 @@ public class Kadai1_1 {
 		System.out.println("好きな食べ物は、油そばです。");
 		System.out.println("太くてもちもちした麺に旨みたっぷりのタレが絡まって、");
 		System.out.println("口の中に入れたときの多幸感がすごいからです！");
+		System.out.println("分かる！最近初めて食べたけどめちゃ美味しいよね！今度一緒に食べに行こう！byめぐ");
 	}
 }
 
